9eb91ca61f162bc962c604b4b997423fbf9107aa
AmandaPradoGit <amandapradogc@gmail.com>
2025-10-11 10:51:33 -0300

    remocao de arquivos desnecessarios

--- app/controller/ProdutoController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'models/Produto.php';
-
-class ProdutoController {
-    public function listar() {
-        $produtos = Produto::listarTodos();
-        include 'views/produto/listar.php';
-    }
-}
